5ffa03a172edc24d2514b7bca15fffb8515a6aad
fabio <invillia.fabio.apolonio@ext.gympass.com>
2021-12-03 11:38:31 -0300

    section 2 - 19 - Component App - with export default

--- fundamentals-react/src/App.js
+++ fundamentals-react/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import First from './components/basics/First';
+import SecondWithParam from './components/basics/SecondWithParam'
+import Fragment from './components/basics/Fragment';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div id="app">
+      <h1>Fundamentos react</h1>
+      <First></First>
+      <SecondWithParam 
+        title="Student Result" 
+        student="Ze" score={9}/>
+      <SecondWithParam 
+        title="Student Result" 
+        student="Jao" score={6.5}/>
+      <Fragment></Fragment>
+   </div>
+ );
 }
 
-export default App;
--- fundamentals-react/src/index.js
+++ fundamentals-react/src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import First from './components/basics/First';
-import SecondWithParam from './components/basics/SecondWithParam'
-import Fragment from './components/basics/Fragment';
+import App from './App';
 
 ReactDOM.render(
-  <div>
-    <First></First>
-    <SecondWithParam 
-      title="Student Result" 
-      student="Ze" score={9}/>
-    <SecondWithParam 
-      title="Student Result" 
-      student="Jao" score={6.5}/>
-    <Fragment></Fragment>
-  </div>
-  ,
+  <App/>,
   document.getElementById('root')
 );
