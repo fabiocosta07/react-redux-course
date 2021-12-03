22f9b9d6be73e65a985d29aa98871bad120b1e8f
fabio <invillia.fabio.apolonio@ext.gympass.com>
2021-12-03 11:13:32 -0300

    section 2 - 18 - React Fragment Error - avoid with React.fragment short version <><.>

--- fundamentals-react/src/components/basics/Fragment.jsx
+++ fundamentals-react/src/components/basics/Fragment.jsx
@@ -2,9 +2,9 @@ import React from "react"
 
 export default function Fragment() {
     return (
-        <React.Fragment>
+        <>
             <h2>Fragment</h2>
             <p>Testing fragment</p>
-        </React.Fragment>
+        </>
     )
 }
