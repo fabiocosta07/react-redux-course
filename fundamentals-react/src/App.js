4eaab9579304d0d16e07c06d985e40bde82a5d2c
fabio <invillia.fabio.apolonio@ext.gympass.com>
2021-12-04 08:55:49 -0300

    section 2 - 24 - component card 03

--- fundamentals-react/src/App.js
+++ fundamentals-react/src/App.js
@@ -9,23 +9,23 @@ export default _ =>
    <div className="App">
       <h1>Fundamentos react</h1>
       <div className="Cards">
-        <Card title="First">
+        <Card title="First" color="#3AB795">
           <First></First>
         </Card>
-        <Card title="Second">
+        <Card title="Second" color="#A0E8AF">
           <SecondWithParam 
             title="Student Result" 
             student="Ze" score={9}/>
         </Card>
-        <Card title="Second with param">
+        <Card title="Second with param" color="#86BAA1">
           <SecondWithParam 
             title="Student Result" 
             student="Jao" score={6.5}/>
         </Card>
-        <Card title="Third Random">
+        <Card title="Third Random" color="#EDEAD0">
           <ThirdRandom min="2" max="200"/>
         </Card>
-        <Card title="Fragment">
+        <Card title="Fragment" color="#ffcf56">
           <Fragment></Fragment>
         </Card>
       </div>
--- fundamentals-react/src/components/basics/layout/Card.jsx
+++ fundamentals-react/src/components/basics/layout/Card.jsx
@@ -2,8 +2,14 @@ import "./Card.css"
 import React from "react"
 
 export default (props) => {
+
+    const cardStyle = {
+        backgroundColor: props.color,
+        borderColor: props.color,
+    }
+
     return (
-        <div className="Card">
+        <div className="Card" style={cardStyle}>
             <div className="Title">{ props.title }</div>
             <div className="Content">
                 { props.children }
