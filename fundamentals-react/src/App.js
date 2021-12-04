9f69cdb665a0cc5757c5103f568724885d8d7e51
fabio <invillia.fabio.apolonio@ext.gympass.com>
2021-12-04 08:14:53 -0300

    section 2 - 22 - component card 01

--- fundamentals-react/src/App.js
+++ fundamentals-react/src/App.js
@@ -3,19 +3,30 @@ import First from './components/basics/First';
 import SecondWithParam from './components/basics/SecondWithParam'
 import Fragment from './components/basics/Fragment';
 import ThirdRandom from './components/basics/ThirdRandom';
+import Card from './components/basics/layout/Card';
 
 export default _ => 
    <div id="app">
-      <h1>Fundamentos react 2</h1>
-      <First></First>
-      <SecondWithParam 
-        title="Student Result" 
-        student="Ze" score={9}/>
-      <SecondWithParam 
-        title="Student Result" 
-        student="Jao" score={6.5}/>
-      <Fragment></Fragment>
-      <ThirdRandom min="2" max="200"/>
+      <h1>Fundamentos react</h1>
+      <Card title="First">
+        <First></First>
+      </Card>
+      <Card title="Second">
+        <SecondWithParam 
+          title="Student Result" 
+          student="Ze" score={9}/>
+      </Card>
+      <Card title="Second with param">
+        <SecondWithParam 
+          title="Student Result" 
+          student="Jao" score={6.5}/>
+      </Card>
+      <Card title="Third Random">
+        <ThirdRandom min="2" max="200"/>
+      </Card>
+      <Card title="Fragment">
+        <Fragment></Fragment>
+      </Card>
    </div>
 
 
